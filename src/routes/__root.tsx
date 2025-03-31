5d34b5f53f2c906d9e638f5d5c5d8db72bff8f1e
Mathias D <mat.decaille@gmail.com>
2025-03-30 22:39:16 -0400

    feat: installed concurrently

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,7 +3,7 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="min-h-screen">
+    <div>
       <Outlet />
       {import.meta.env.DEV && <TanStackRouterDevtools />}
     </div>
