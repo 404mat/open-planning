f458f474d3aedc3e21423aec08fb6a3291186726
Mathias D <mat.decaille@gmail.com>
2025-04-11 15:24:27 -0400

    add a few numbers to the fibonacci suite

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -11,7 +11,7 @@ export const TABLE_HEIGHT = 180;
 
 /* voting system types */
 // prettier-ignore
-export const FIBONACCI_SYSTEM = ['1', '2', '3', '5', '8', '13', '21', '❓', '☕'];
+export const FIBONACCI_SYSTEM = ['1', '2', '3', '5', '8', '13', '21', '34', '144', '❓', '☕'];
 // prettier-ignore
 export const NUMBERS_SYSTEM = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '❓', '🗣️'];
 // prettier-ignore
