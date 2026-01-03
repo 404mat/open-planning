df49ae49880abbd4c24c584179225943807b9c83
Mathias D <mat.decaille@gmail.com>
2026-01-03 18:58:17 -0500

    adding log out button (not working)

--- src/components/name-avatar.tsx
+++ src/components/name-avatar.tsx
@@ -1,11 +1,37 @@
 import Avvvatars from 'avvvatars-react';
 import { Button } from './ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
 
-export default function NameAvatar({ userName }: { userName?: string }) {
+export default function NameAvatar({
+  userName,
+  onLogout,
+}: {
+  userName?: string;
+  onLogout?: () => void;
+}) {
   return (
-    <Button className="gap-3 rounded-full px-3 ps-2">
-      <Avvvatars value={userName ?? ''} style="shape" size={22} />
-      {userName}
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="gap-3 rounded-full px-3 ps-2">
+          <Avvvatars value={userName ?? ''} style="shape" size={22} />
+          {userName}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem
+          variant="destructive"
+          onClick={() => {
+            onLogout?.();
+          }}
+        >
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
